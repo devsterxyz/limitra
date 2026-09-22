415b9a25a4fa3d9d26e61273ef06f71a63be38d1
Dev <sharmadevdeep9@gmail.com>
2026-09-22 19:28:13 +0530

    docker setup

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const redis = createClient({
-  url: "redis://localhost:6379",
+  url: process.env.REDIS_URL ? process.env.REDIS_URL : "redis://localhost:6379",
 });
 
 redis.on("error", (error) => {
